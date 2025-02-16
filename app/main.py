c3fb6010583801e8dacde071797faf927c68e6bb
Rueben Engelhart <engelhartrueben@gmail.com>
2025-02-15 21:18:45 -0500

    add some models. clean up. more outline

--- app/main.py
+++ app/main.py
@@ -6,29 +6,52 @@ from datetime import datetime
 
 app = FastAPI()
 
-# Connect to existing database
-
-# Auto commit
-conn.set_session(autocommit=True)
-
-# Open a cursor to perform database operations
-curr = conn.cursor()
-
+# Yields unique id.
+# probably not needed. like ever. but its a generator
+# so oh well.
 def id_gen():
     num = 1
     yield num
     num += 1
 
+# Basic success message
+class Default(BaseModel):
+    "message": "success"
+
+# Request total hours worked
+class ReqTotalHours(BaseModel):
+    "auth": str
+
+# Return total hours worked. Greedy
+class RetTotalHours(BaseModel):
+    "user_id": int
+    "total_hours_worked": float
+
+# Request total hours worked between two days
+class ReqTotalHoursInRange(BaseModel):
+    "start_date": datetime
+    "end_date": datetime
+    "auth": str
+
+# Returns the dates requested, and the total amount
+# of time in integer minutes
+class RetTotalHoursInrange(BaseModel):
+    "start_date": datetime
+    "end_date": datetime
+    "total_minutes": int
+
 @app.get("/")
-def default_response():
+def default() -> Default:
+    return { "message": "success" }
+
+@app.get("/timesheet/{user_id}")
+def get_total_time(
+        user_id: int, 
+) -> TotalHours:
     pass
 
 @app.get("/timesheet/{user_id}")
-def get_total_time(user_id: str):
-    pass
-
-@app.get("/timesheet/clock/{start_day}/{end_day}")
-def get_time_in_range(range: str):
+def get_time_in_range(user_id: int, req: ):
     pass
 
 @app.get("/timesheet/clock/last/{user_id}")
